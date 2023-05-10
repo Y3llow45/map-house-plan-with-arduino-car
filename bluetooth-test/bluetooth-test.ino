a7a99b75e19306de8ea6b7b913619936c06cf005
Yellow <bofengejn@gmail.com>
2023-05-10 21:41:49 +0300

    Create bluetooth-test.ino

---
+++ bluetooth-test/bluetooth-test.ino
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>
+SoftwareSerial HM10(0, 1);
+char appData;  
+String inData = "";
+const int motorPin1  = 5;
+const int motorPin2  = 6;
+const int motorPin3  = 7;
+const int motorPin4  = 8;
+bool forward = false;
+bool right = false;
+bool backward = false;
+bool left = false;
+
+void setup(){
+  Serial.begin(9600);
+  HM10.begin(9600);
+}
+
+void loop(){
+  HM10.listen();
+  while (HM10.available() > 0) {
+    appData = HM10.read();
+    inData = String(appData);
+  }
+  if (Serial.available()) {
+    delay(10);
+    HM10.write(Serial.read());
+  }
+  if ( inData == "A") {
+    digitalWrite(8, HIGH);
+    delay(100);
+  }
+  if ( inData == "B") {
+    digitalWrite(8, LOW);
+    delay(100);
+  }
+  switch(inData) {
+    case "W":
+      forward
+      break;
+  }
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(2000);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(2000);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    delay(2000);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    delay(2000);
+}
