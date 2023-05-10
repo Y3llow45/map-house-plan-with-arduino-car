a188365bc74d8610be852b156c48abd5594eeda0
Yellow <bofengejn@gmail.com>
2023-05-10 22:17:37 +0300

    Update bluetooth-test.ino

--- bluetooth-test/bluetooth-test.ino
+++ bluetooth-test/bluetooth-test.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 SoftwareSerial HM10(0, 1);
 char appData;  
-String inData = "";
+char inData;
 const int motorPin1  = 5;
 const int motorPin2  = 6;
 const int motorPin3  = 7;
@@ -14,18 +14,54 @@ bool left = false;
 void setup(){
   Serial.begin(9600);
   HM10.begin(9600);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 }
 
 void loop(){
   HM10.listen();
   while (HM10.available() > 0) {
     appData = HM10.read();
-    inData = String(appData);
+    inData = appData;
+    Serial.print(inData);
   }
   if (Serial.available()) {
     delay(10);
     HM10.write(Serial.read());
   }
+  if(inData == 'W'){
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    delay(10);
+  }
+  if(inData == 'D'){
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    delay(10);
+  }
+  if(inData == 'S'){
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    delay(10);
+  }
+  if(inData == 'A'){
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    delay(10);
+  }
+}
+
+void previous() {
   if ( inData == "W") {
     forward = true;
     right = false;
@@ -50,29 +86,32 @@ void loop(){
     backward = false;
     left = true;
   }
-  if (forward) {
+  if (forward == true) {
     digitalWrite(motorPin1, HIGH);
     digitalWrite(motorPin2, LOW);
     digitalWrite(motorPin3, HIGH);
     digitalWrite(motorPin4, LOW);
-    
+    delay(1000);
   }
-  if (right) {
+  if (right == true) {
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, HIGH);
     digitalWrite(motorPin3, HIGH);
     digitalWrite(motorPin4, LOW);
+    delay(1000);
   }
-  if (backward) {
+  if (backward == true) {
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, HIGH);
     digitalWrite(motorPin3, LOW);
     digitalWrite(motorPin4, HIGH);
+    delay(1000);
   }
-  if (left) {
+  if (left == true) {
     digitalWrite(motorPin1, HIGH);
     digitalWrite(motorPin2, LOW);
     digitalWrite(motorPin3, LOW);
     digitalWrite(motorPin4, HIGH);
-  }
+    delay(1000);
+  }  
 }
